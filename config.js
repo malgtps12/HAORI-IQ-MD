8391348fb639b301c1c4a64cba54530a35f7e7b7
malgtps12 <93052305+malgtps12@users.noreply.github.com>
2022-09-30 15:16:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ let fetch = require('node-fetch')
  function pickRandom(list) { 
    return list[Math.floor(Math.random() * list.length)] 
  } 
- global.owner = ['6285158866902','62831433937633', '62896255561617'] // Put your number here 
+ global.owner = ['6289516353968'] // Put your number here 
  global.mods = [] // Want some help? 
  global.prems = [] // Premium user has unlimited limit 
  global.APIs = { // API Prefix 
@@ -62,19 +62,19 @@ let fetch = require('node-fetch')
  // Sticker WM 
  global.stiker_wait = '*WAIT! | Mohon Tunggu Sebentar...*' 
  global.packname = 'Buatan Siapa?' 
- global.author = `Buatan Haori` 
- global.wm = 'ＨＡＯＲＩ　ＩＱ　ＭＤ　🌹　'  
+ global.author = `Buatan MallBot-MD` 
+ global.wm = 'MallBot-MD' 
  global.watermark = wm 
- global.linkgc = 'https://chat.whatsapp.com/Kyz397uqvJZ5AvX7I4BSID'
-global.link = 'https://youtu.be/0CyJP3EKrf4'
+ global.linkgc = '-'
+global.link = '-'
 global.deslink = ''
-global.titlink = 'ＨＡＯＲＩ　ＩＱ　ＭＤ　🌹　'
-global.bodlink = '@zivfurr'
-global.linkig = 'https://instagram.com/not_pilar32'
+global.titlink = 'MallBot-MD'
+global.bodlink = '@xzdk.m'
+global.linkig = 'https://instagram.com/xzdk.m'
 global.instagram = 'https://instagram.com/not_pilar32'
  global.botdate = `❏ ${wktuh} H  ${wktum} M  ${wktus} S\n❏ ${week} ${date}` 
  global.media = 'https://telegra.ph/file/ccb2359d3925fd7af58cd.jpg' 
- global.github = 'https://github.com/PilarV2' 
+ global.github = 'https://github.com/malgtps' 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23401620&text=' 
  global.wait = '_*L O A D I N G*_' 
  global.eror = '_*Server Error*_' 
